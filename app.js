a8e986c8851e477a46c9fa8f882c5e79be21db34
emmaodia <odia.emma@gmail.com>
2020-02-20 13:05:19 +0100

    Ready!

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ app.get('/', (req, res) => res.json(
     {msg: "Welcome, Nerd!"}
 ));
 
+//Database Configuration
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/enterpair-api";
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect( url , {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+      console.log("Successfully connected to the database");
+  }).catch(err => {
+      console.log('Could not connect to the database. Exiting now...');
+      console.log(err)
+      process.exit();
+  });
+
 const port = 5000;
 app.set(port)
 
